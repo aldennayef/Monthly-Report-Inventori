468132deee4bb55da8bc27efaa63691cf431c107
aldennayef <aldennayef@gmail.com>
2024-08-15 11:50:57 +0700

    ok

--- application/views/inventori/v_clusters.php
+++ application/views/inventori/v_clusters.php
@@ -77,3 +77,145 @@
             </div>
             <!-- /.content-wrapper -->
             
+            <!--CALL FOOTER-->
+            <footer class="main-footer">
+                <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+                All rights reserved.
+                <div class="float-right d-none d-sm-inline-block">
+                <b>Version</b> 3.2.0
+                </div>
+            </footer>
+
+        </div>
+        <!-- ./wrapper -->
+
+        <!-- jQuery -->
+        <script src="<?=base_url('assets/plugins/jquery/jquery.min.js')?>"></script>
+        <!-- Bootstrap 4 -->
+        <script src="<?=base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
+        <!-- DataTables  & Plugins -->
+        <script src="<?=base_url('assets/plugins/datatables/jquery.dataTables.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/datatables-buttons/js/dataTables.buttons.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/jszip/jszip.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/pdfmake/pdfmake.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/pdfmake/vfs_fonts.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/datatables-buttons/js/buttons.html5.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/datatables-buttons/js/buttons.print.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/datatables-buttons/js/buttons.colVis.min.js')?>"></script>
+        <!-- daterangepicker -->
+        <script src="<?=base_url('assets/plugins/moment/moment.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/daterangepicker/daterangepicker.js')?>"></script>
+        <!-- Summernote -->
+        <script src="<?=base_url('assets/plugins/summernote/summernote-bs4.min.js')?>"></script>
+        <!-- Sparkline -->
+        <script src="<?=base_url('assets/plugins/sparklines/sparkline.js')?>"></script>
+        <!-- JQVMap -->
+        <script src="<?=base_url('assets/plugins/jqvmap/jquery.vmap.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/jqvmap/maps/jquery.vmap.usa.js')?>"></script>
+        <!-- jQuery Knob Chart -->
+        <script src="<?=base_url('assets/plugins/jquery-knob/jquery.knob.min.js')?>"></script>
+        <!-- overlayScrollbars -->
+        <script src="<?=base_url('assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')?>"></script>
+        <!-- AdminLTE App -->
+        <script src="<?=base_url('assets/dist/js/adminlte.js')?>"></script>
+        <!-- AdminLTE for demo purposes -->
+        <script src="<?=base_url('assets/dist/js/demo.js')?>"></script>
+        <!-- SweetAlert2 -->
+        <script src="<?=base_url('assets/plugins/sweetalert2/sweetalertnotif2.min.js')?>"></script>
+
+        <script>
+            $(function () {
+                    $('#item').DataTable({
+                    "paging": true,
+                    "lengthChange": false,
+                    "searching": false,
+                    "ordering": true,
+                    "info": true,
+                    "autoWidth": false,
+                    "responsive": true,
+                    });
+                });
+                $(document).ready(function() {
+                    // Event listener untuk tombol edit
+                    $('.edit-btn').click(function() {
+                        var row = $(this).closest('tr'); // Dapatkan baris yang sesuai dengan tombol yang diklik
+                        var clusterText = row.find('.cluster-text').text(); // Ambil teks cluster yang ada
+                        var kodeclusterText = row.find('.kode-cluster-text').text(); // Ambil teks cluster yang ada
+                        row.find('.cluster-text').hide(); // Sembunyikan teks cluster
+                        row.find('.cluster-input').val(clusterText).show(); // Tampilkan input dengan nilai yang ada
+                        row.find('.kode-cluster-text').hide(); // Sembunyikan teks cluster
+                        row.find('.kode-cluster-input').val(kodeclusterText).show(); // Tampilkan input dengan nilai yang ada
+                        $(this).hide(); // Sembunyikan tombol edit
+                        row.find('.lock-btn, .cancel-btn').show(); // Tampilkan tombol lock dan cancel
+                    });
+
+                    // Event listener untuk tombol lock
+                    $('.lock-btn').click(function() {
+                        var row = $(this).closest('tr');
+                        var updatedClusterText = row.find('.cluster-input').val(); // Ambil nilai dari input
+                        var oldClusterText = row.find('.cluster-old').val(); // Ambil nilai dari input
+                        var kodeupdatedClusterText = row.find('.kode-cluster-input').val(); // Ambil nilai dari input
+                        var kodeoldClusterText = row.find('.kode-cluster-old').val(); // Ambil nilai dari input
+                        var type_input = row.find('.type-input').val(); // Ambil nilai dari input
+                        var clusterId = row.data('clusterid');
+                        $.ajax({
+                            url: '<?= base_url('act') ?>',  // Ganti dengan URL yang sesuai
+                            method: 'POST',
+                            data: {
+                                id_cluster: clusterId,
+                                nama_cluster: updatedClusterText,
+                                nama_cluster_old: oldClusterText,
+                                kode_cluster: kodeupdatedClusterText,
+                                kode_cluster_old: kodeoldClusterText,
+                                type: type_input,
+                            },
+                            success: function(response) {
+                                if (response.status === 'success') {
+                                    // Jika berhasil, perbarui tampilan
+                                    row.find('.cluster-text').text(updatedClusterText).show();
+                                    row.find('.cluster-input').hide();
+                                    row.find('.kode-cluster-text').text(kodeupdatedClusterText).show();
+                                    row.find('.kode-cluster-input').hide();
+                                    row.find('.lock-btn, .cancel-btn').hide();
+                                    row.find('.edit-btn').show();
+                                } 
+                                else if(response.status === 'duplicate'){
+                                    Swal.fire({
+                                        icon: 'error',
+                                        title: 'Gagal!',
+                                        text: 'Kode Sudah Ada !',
+                                    });
+                                }
+                                else {
+                                    alert('Update gagal: ' + response.message);
+                                }
+                            },
+                            error: function() {
+                                alert('Terjadi kesalahan saat mengirim data.');
+                            }
+                        });
+                        // Lakukan AJAX untuk menyimpan nilai baru di server jika perlu
+                        // row.find('.cluster-text').text(updatedClusterText).show(); // Perbarui dan tampilkan teks cluster baru
+                        // row.find('.cluster-input').hide(); // Sembunyikan input cluster
+                        // $(this).hide(); // Sembunyikan tombol lock
+                        // row.find('.cancel-btn').hide(); // Sembunyikan tombol cancel
+                        // row.find('.edit-btn').show(); // Tampilkan kembali tombol edit
+                    });
+
+                    // Event listener untuk tombol cancel
+                    $('.cancel-btn').click(function() {
+                        var row = $(this).closest('tr');
+                        row.find('.cluster-input').hide(); // Sembunyikan input cluster
+                        row.find('.cluster-text').show(); // Tampilkan kembali teks cluster
+                        row.find('.kode-cluster-input').hide(); // Sembunyikan input cluster
+                        row.find('.kode-cluster-text').show(); // Tampilkan kembali teks cluster
+                        $(this).hide(); // Sembunyikan tombol cancel
+                        row.find('.lock-btn').hide(); // Sembunyikan tombol lock
+                        row.find('.edit-btn').show(); // Tampilkan kembali tombol edit
+                    });
+                });
+        </script>
--- application/views/inventori/v_users.php
+++ application/views/inventori/v_users.php
@@ -75,59 +75,118 @@
             </div>
             <!-- /.content-wrapper -->
 
-            <script src="<?=base_url('assets/plugins/jquery/jquery.min.js')?>"></script>
+            <!--CALL FOOTER-->
+            <footer class="main-footer">
+                <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+                All rights reserved.
+                <div class="float-right d-none d-sm-inline-block">
+                <b>Version</b> 3.2.0
+                </div>
+            </footer>
+
+        </div>
+        <!-- ./wrapper -->
+
+        <!-- jQuery -->
+        <script src="<?=base_url('assets/plugins/jquery/jquery.min.js')?>"></script>
+        <!-- Bootstrap 4 -->
+        <script src="<?=base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
+        <!-- DataTables  & Plugins -->
+        <script src="<?=base_url('assets/plugins/datatables/jquery.dataTables.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/datatables-buttons/js/dataTables.buttons.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/jszip/jszip.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/pdfmake/pdfmake.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/pdfmake/vfs_fonts.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/datatables-buttons/js/buttons.html5.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/datatables-buttons/js/buttons.print.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/datatables-buttons/js/buttons.colVis.min.js')?>"></script>
+        <!-- daterangepicker -->
+        <script src="<?=base_url('assets/plugins/moment/moment.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/daterangepicker/daterangepicker.js')?>"></script>
+        <!-- Summernote -->
+        <script src="<?=base_url('assets/plugins/summernote/summernote-bs4.min.js')?>"></script>
+        <!-- Sparkline -->
+        <script src="<?=base_url('assets/plugins/sparklines/sparkline.js')?>"></script>
+        <!-- JQVMap -->
+        <script src="<?=base_url('assets/plugins/jqvmap/jquery.vmap.min.js')?>"></script>
+        <script src="<?=base_url('assets/plugins/jqvmap/maps/jquery.vmap.usa.js')?>"></script>
+        <!-- jQuery Knob Chart -->
+        <script src="<?=base_url('assets/plugins/jquery-knob/jquery.knob.min.js')?>"></script>
+        <!-- overlayScrollbars -->
+        <script src="<?=base_url('assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')?>"></script>
+        <!-- AdminLTE App -->
+        <script src="<?=base_url('assets/dist/js/adminlte.js')?>"></script>
+        <!-- AdminLTE for demo purposes -->
+        <script src="<?=base_url('assets/dist/js/demo.js')?>"></script>
+        <!-- SweetAlert2 -->
+        <script src="<?=base_url('assets/plugins/sweetalert2/sweetalertnotif2.min.js')?>"></script>
 
             <script>
-                $(document).ready(function() {
-                $('.edit-user-btn').click(function() {
-                    var row = $(this).closest('tr'); // Dapatkan baris yang sesuai dengan tombol yang diklik
-                    row.find('.cluster-text').hide(); // Sembunyikan teks cluster
-                    row.find('.cluster-select').show(); // Tampilkan dropdown cluster
-                    $(this).hide(); // Sembunyikan tombol edit
-                    row.find('.lock-user-btn, .cancel-user-btn').show(); // Tampilkan tombol lock dan cancel
-                });
-
-                // Event listener untuk tombol lock
-                $('.lock-user-btn').click(function() {
-                    var row = $(this).closest('tr');
-                    var nik = row.data('nik'); // Dapatkan NIK dari data attribute
-                    var selectedClusterValue = row.find('.cluster-select').val(); // Ambil nilai cluster yang dipilih
-                    var namauserValue = row.find('.nama-user').val(); // Ambil nilai cluster yang dipilih
-
-                    // Lakukan AJAX untuk menyimpan nilai baru di server
-                    $.ajax({
-                        url: '<?= base_url("inventori/proses/aksi_users") ?>', // Ubah URL ke controller aksi_users
-                        method: 'POST',
-                        data: {
-                            nik: nik,
-                            nama_user: namauserValue,
-                            id_cluster: selectedClusterValue
-                        },
-                        success: function(response) {
-                            if (response.status === 'success_add' || response.status === 'success_update') {
-                                var selectedClusterText = row.find('.cluster-select option:selected').text(); // Ambil teks cluster yang dipilih
-                                row.find('.cluster-text').text(selectedClusterText).show(); // Perbarui dan tampilkan teks cluster baru
-                                row.find('.cluster-select').hide(); // Sembunyikan dropdown cluster
-                                row.find('.lock-user-btn, .cancel-user-btn').hide(); // Sembunyikan tombol lock dan cancel
-                                row.find('.edit-user-btn').show(); // Tampilkan kembali tombol edit
-                            } else {
-                                alert('Update gagal!');
-                            }
-                        },
-                        error: function() {
-                            alert('Terjadi kesalahan dalam pengiriman data.');
-                        }
+                $(function () {
+                    $('#item').DataTable({
+                    "paging": true,
+                    "lengthChange": false,
+                    "searching": false,
+                    "ordering": true,
+                    "info": true,
+                    "autoWidth": false,
+                    "responsive": true,
                     });
                 });
+                $(document).ready(function() {
+                    $('.edit-user-btn').click(function() {
+                        var row = $(this).closest('tr'); // Dapatkan baris yang sesuai dengan tombol yang diklik
+                        row.find('.cluster-text').hide(); // Sembunyikan teks cluster
+                        row.find('.cluster-select').show(); // Tampilkan dropdown cluster
+                        $(this).hide(); // Sembunyikan tombol edit
+                        row.find('.lock-user-btn, .cancel-user-btn').show(); // Tampilkan tombol lock dan cancel
+                    });
 
-                // Event listener untuk tombol cancel
-                $('.cancel-user-btn').click(function() {
-                    var row = $(this).closest('tr');
-                    row.find('.cluster-select').hide(); // Sembunyikan dropdown cluster
-                    row.find('.cluster-text').show(); // Tampilkan kembali teks cluster
-                    $(this).hide(); // Sembunyikan tombol cancel
-                    row.find('.lock-user-btn').hide(); // Sembunyikan tombol lock
-                    row.find('.edit-user-btn').show(); // Tampilkan kembali tombol edit
-                });
+                    // Event listener untuk tombol lock
+                    $('.lock-user-btn').click(function() {
+                        var row = $(this).closest('tr');
+                        var nik = row.data('nik'); // Dapatkan NIK dari data attribute
+                        var selectedClusterValue = row.find('.cluster-select').val(); // Ambil nilai cluster yang dipilih
+                        var namauserValue = row.find('.nama-user').val(); // Ambil nilai cluster yang dipilih
+
+                        // Lakukan AJAX untuk menyimpan nilai baru di server
+                        $.ajax({
+                            url: '<?= base_url("inventori/proses/aksi_users") ?>', // Ubah URL ke controller aksi_users
+                            method: 'POST',
+                            data: {
+                                nik: nik,
+                                nama_user: namauserValue,
+                                id_cluster: selectedClusterValue
+                            },
+                            success: function(response) {
+                                if (response.status === 'success_add' || response.status === 'success_update') {
+                                    var selectedClusterText = row.find('.cluster-select option:selected').text(); // Ambil teks cluster yang dipilih
+                                    row.find('.cluster-text').text(selectedClusterText).show(); // Perbarui dan tampilkan teks cluster baru
+                                    row.find('.cluster-select').hide(); // Sembunyikan dropdown cluster
+                                    row.find('.lock-user-btn, .cancel-user-btn').hide(); // Sembunyikan tombol lock dan cancel
+                                    row.find('.edit-user-btn').show(); // Tampilkan kembali tombol edit
+                                } else {
+                                    alert('Update gagal!');
+                                }
+                            },
+                            error: function() {
+                                alert('Terjadi kesalahan dalam pengiriman data.');
+                            }
+                        });
+                    });
+
+                    // Event listener untuk tombol cancel
+                    $('.cancel-user-btn').click(function() {
+                        var row = $(this).closest('tr');
+                        row.find('.cluster-select').hide(); // Sembunyikan dropdown cluster
+                        row.find('.cluster-text').show(); // Tampilkan kembali teks cluster
+                        $(this).hide(); // Sembunyikan tombol cancel
+                        row.find('.lock-user-btn').hide(); // Sembunyikan tombol lock
+                        row.find('.edit-user-btn').show(); // Tampilkan kembali tombol edit
+                    });
                 });
             </script>
